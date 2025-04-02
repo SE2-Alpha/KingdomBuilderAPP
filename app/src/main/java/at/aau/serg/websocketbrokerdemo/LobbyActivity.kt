a4c5e1d814a0cc5548d07f3649f493306f2f96b2
noemi-presser <noemihannah.presser@gmail.com>
2025-04-03 00:51:39 +0200

    created very basic layout in LobbyActivity.kt

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/LobbyActivity.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/LobbyActivity.kt
@@ -1,17 +1,39 @@
 package at.aau.serg.websocketbrokerdemo
 
+import android.R.attr.id
+import android.inputmethodservice.Keyboard.Row
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import at.aau.serg.websocketbrokerdemo.ui.theme.MyApplicationTheme
+import com.example.myapplication.R
 
 class LobbyActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +48,80 @@ class LobbyActivity : ComponentActivity() {
 @Composable
 fun Lobby() {
 
+    Box (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .background(colorResource(id = R.color.beige_lobby_background))
+    ){
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+        ) {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .padding(8.dp)
+                    .height(80.dp)
+                    .width(170.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = R.color.light_blue_900),
+                    contentColor = Color.White
+                ),
+                shape = RoundedCornerShape(16.dp)
+            ) {
+                Text(text = stringResource(id = R.string.lets_go))
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+        ) {
+
+            //für Settings und Regeln Composable functions oder eigene Acitvities??
+
+            Column {
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .height(40.dp)
+                        .width(100.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(id = R.color.light_blue_900),
+                        contentColor = Color.White
+                    ),
+                    shape = RoundedCornerShape(16.dp)
+                ) {
+                    Text(text = stringResource(id = R.string.settings))
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .height(40.dp)
+                        .width(100.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(id = R.color.light_blue_900),
+                        contentColor = Color.White
+                    ),
+                    shape = RoundedCornerShape(16.dp)
+                ) {
+                    Text(text = stringResource(id = R.string.rules))
+                }
+            }
+        }
+    }
+
+    /*Image(
+        painter = painterResource(id = R.drawable.lobbyactivity),
+        contentDescription = "Background Image",
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop
+    )
+     */
 }
 
 @Preview(showBackground = true)
