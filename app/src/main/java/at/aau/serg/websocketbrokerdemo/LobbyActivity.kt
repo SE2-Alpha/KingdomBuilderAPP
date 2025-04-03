a2f63f13972f9deeee4118e8a8d47903ecff3c47
noemi-presser <noemihannah.presser@gmail.com>
2025-04-03 10:03:13 +0200

    LobbyActivity.kt -> added background and title, adapted buttons

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/LobbyActivity.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/LobbyActivity.kt
@@ -1,5 +1,7 @@
 package at.aau.serg.websocketbrokerdemo
 
+import android.R.attr.fontStyle
+import android.R.attr.fontWeight
 import android.R.attr.id
 import android.inputmethodservice.Keyboard.Row
 import android.os.Bundle
@@ -15,7 +17,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -25,13 +29,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import at.aau.serg.websocketbrokerdemo.ui.theme.MyApplicationTheme
 import com.example.myapplication.R
 
@@ -51,9 +58,24 @@ fun Lobby() {
     Box (
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
-            .background(colorResource(id = R.color.beige_lobby_background))
     ){
+        // Hintergrundbild
+        Image(
+            painter = painterResource(id = R.drawable.lobbyactivity),
+            contentDescription = "Background Image",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.titel),
+            contentDescription = "Small Icon",
+            modifier = Modifier
+                .offset(x = 150.dp, y = (-30).dp)
+                .size(230.dp)
+                .align(Alignment.TopStart)
+        )
+
 
         Column(
             modifier = Modifier
@@ -64,14 +86,19 @@ fun Lobby() {
                 modifier = Modifier
                     .padding(8.dp)
                     .height(80.dp)
-                    .width(170.dp),
+                    .width(170.dp)
+                    .shadow(8.dp, shape = RoundedCornerShape(20.dp)),
                 colors = ButtonDefaults.buttonColors(
-                    containerColor = colorResource(id = R.color.light_blue_900),
-                    contentColor = Color.White
+                    containerColor = colorResource(id = R.color.beige_lobby_background),
+                    contentColor = colorResource(id = R.color.light_blue_900)
                 ),
-                shape = RoundedCornerShape(16.dp)
+                shape = RoundedCornerShape(20.dp)
             ) {
-                Text(text = stringResource(id = R.string.lets_go))
+                Text(
+                    text = stringResource(id = R.string.lets_go),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
             }
         }
 
@@ -88,12 +115,13 @@ fun Lobby() {
                     modifier = Modifier
                         .padding(8.dp)
                         .height(40.dp)
-                        .width(100.dp),
+                        .width(100.dp)
+                        .shadow(8.dp, shape = RoundedCornerShape(20.dp)),
                     colors = ButtonDefaults.buttonColors(
-                        containerColor = colorResource(id = R.color.light_blue_900),
-                        contentColor = Color.White
+                        containerColor = colorResource(id = R.color.beige_lobby_background),
+                        contentColor = colorResource(id = R.color.light_blue_900)
                     ),
-                    shape = RoundedCornerShape(16.dp)
+                    shape = RoundedCornerShape(20.dp)
                 ) {
                     Text(text = stringResource(id = R.string.settings))
                 }
@@ -102,12 +130,13 @@ fun Lobby() {
                     modifier = Modifier
                         .padding(8.dp)
                         .height(40.dp)
-                        .width(100.dp),
+                        .width(100.dp)
+                        .shadow(8.dp, shape = RoundedCornerShape(20.dp)),
                     colors = ButtonDefaults.buttonColors(
-                        containerColor = colorResource(id = R.color.light_blue_900),
-                        contentColor = Color.White
+                        containerColor = colorResource(id = R.color.beige_lobby_background),
+                        contentColor = colorResource(id = R.color.light_blue_900)
                     ),
-                    shape = RoundedCornerShape(16.dp)
+                    shape = RoundedCornerShape(20.dp)
                 ) {
                     Text(text = stringResource(id = R.string.rules))
                 }
