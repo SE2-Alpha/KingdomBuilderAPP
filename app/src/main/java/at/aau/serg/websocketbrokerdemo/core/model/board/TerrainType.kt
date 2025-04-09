7f19404428897b215527d621391f50450423f4d1
Viktoris <viktoris@edu.aau.at>
2025-04-09 22:26:12 +0200

    Refacotring smelly TerrainType

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/core/model/board/TerrainType.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/core/model/board/TerrainType.kt
@@ -5,6 +5,19 @@ package at.aau.serg.websocketbrokerdemo.core.model.board
  * @property isBuildable Kann auf diesem Feld gebaut werden?
  */
 
-sealed interface TerrainType{
-    val isBuildable: Boolean
+enum class TerrainType( val isBuildable: Boolean = true) {
+    GRASS,  // Grasland
+    CANYON, // Schlucht
+    DESERT, // Wüste
+    FLOWERS,// Blumenfeld
+    FOREST,  // Wald
+    WATER(false),   // Wasser (Verschiebeaktionen möglich)
+    MOUNTAIN(false), // Berg (unpassierbar)
+    SPECIALABILITY(false), //alle Spezialfähigkeiten
+    CITY(false); //alle Städte
+
+    /**
+     * For the special ability
+     */
+    fun allowsMovement() = this == WATER
 }
