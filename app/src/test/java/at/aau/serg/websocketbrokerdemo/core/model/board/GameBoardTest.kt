6f3cd9e864bf61ca2633beefe91d71216962a6b6
leona <leonardotocci6@gmail.com>
2025-04-25 12:04:12 +0200

    Implemented TerrainTypeTest, expanded TerrainFieldTest

--- app/src/test/java/at/aau/serg/websocketbrokerdemo/core/model/board/GameBoardTest.kt
+++ app/src/test/java/at/aau/serg/websocketbrokerdemo/core/model/board/GameBoardTest.kt
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.params.provider.EnumSource
-import org.mockito.Mockito.*
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
--- app/src/test/java/at/aau/serg/websocketbrokerdemo/core/model/board/TerrainFieldTest.kt
+++ app/src/test/java/at/aau/serg/websocketbrokerdemo/core/model/board/TerrainFieldTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
+import org.mockito.Mockito.mock
 
 class TerrainFieldTest {
     private lateinit var terrainField: TerrainField
@@ -18,10 +19,26 @@ class TerrainFieldTest {
     }
 
     @ParameterizedTest
-    @EnumSource(TerrainType::class)
+    @EnumSource(value = TerrainType::class, names = ["GRASS", "CANYON", "DESERT", "FLOWERS", "FOREST",])
     fun isBuildableTrueTest(type: TerrainType){
-        terrainField = TerrainField(type,5)
+        terrainField = TerrainField(type,22)
+        assertNull(terrainField.builtBy)
         assertTrue(terrainField.isBuildable)
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = TerrainType::class, names =["WATER", "MOUNTAIN", "SPECIALABILITY", "CITY"])
+    fun isBuildableFalseTest(type: TerrainType){
+        terrainField = TerrainField(type,5)
+        assertFalse(terrainField.isBuildable)
+    }
+
+    @Test
+    fun isBuildableFalseOccupiedTest(){
+        player = mock()
+        terrainField = TerrainField(TerrainType.GRASS,5)
+        terrainField.builtBy = player
+        assertFalse(terrainField.isBuildable)
     }
 
 
