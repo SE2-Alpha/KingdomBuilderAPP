9880ea81f17911724417640e0fefabdfcd4e723e
Leonardo Tocci <leonardotocci6@gmail.com>
2025-04-28 11:55:13 +0200

    Fixed Test in GameBoardTest

--- app/src/test/java/at/aau/serg/websocketbrokerdemo/core/model/board/GameBoardTest.kt
+++ app/src/test/java/at/aau/serg/websocketbrokerdemo/core/model/board/GameBoardTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.params.provider.EnumSource
+import org.mockito.Mockito.mock
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
@@ -33,6 +34,8 @@ class GameBoardTest {
 
     @Test
     fun areFieldsAdjacentTest(){
+        field1 = mock()
+        field2 = mock()
         assertFailsWith<NotImplementedError> {
             gameBoardTest.areFieldsAdjacent(field1, field2)
         }
