b7222f82491fdf857d3948fc27150c5d3ad146b5
Viktoris <viktoris@edu.aau.at>
2025-04-29 09:07:23 +0200

    TerrainField map out neighbours for all fields

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/core/model/board/TerrainField.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/core/model/board/TerrainField.kt
@@ -23,6 +23,22 @@ class TerrainField(val type: TerrainType, val id: Int) { //needs quadrant number
     val isBuildable: Boolean
         get() = type.isBuildable && builtBy == null
 
+    fun getNeighbours(id: Int): Int{
+        return when(id){
+            (40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320, 340, 360) -> 5
+            (39, 59, 79, 99, 119, 139, 159, 179, 199, 219, 23, 259, 279, 299, 319, 339, 359, 379) -> 5
+            (id > 20 && id < 380) -> 6
+            id > 0 && id < 19 -> 4
+            id > 380 && id < 400 -> 4
+            0 -> 2
+            20 -> 3
+            380 -> 3
+            400 -> 2
+
+            else -> {}
+        } as Int
+    }
+
     fun getColor(context: Context): Color {
             return Color(when (type) {
                 TerrainType.GRASS -> ContextCompat.getColor(context, R.color.terrain_grass)
