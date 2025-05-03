c924ba0c8c7be3ca3f8ce372d6f4be543acafab6
Viktoris <viktoris@edu.aau.at>
2025-05-03 18:43:36 +0200

    TerrainField getNeighbours returns Array of Ints

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/core/model/board/TerrainField.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/core/model/board/TerrainField.kt
@@ -23,8 +23,8 @@ class TerrainField(val type: TerrainType, val id: Int) { //needs quadrant number
     val isBuildable: Boolean
         get() = type.isBuildable && builtBy == null
 
-    fun getNeighbours(id: Int): Int{
-        return when(id){
+    fun getNeighbours(id: Int): Array<Int>{
+        when(id){
             (40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320, 340, 360) -> 5
             (39, 59, 79, 99, 119, 139, 159, 179, 199, 219, 23, 259, 279, 299, 319, 339, 359, 379) -> 5
             (id > 20 && id < 380) -> 6
