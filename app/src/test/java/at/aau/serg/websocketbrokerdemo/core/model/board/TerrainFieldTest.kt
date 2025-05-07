50f4b0e9cf2ada56c2543f948ca27b26adb5e853
Viktoris <viktoris@edu.aau.at>
2025-05-07 22:18:35 +0200

    Fix getNeighboursAmountTest for ids not inside the Gameboard

--- app/src/test/java/at/aau/serg/websocketbrokerdemo/core/model/board/TerrainFieldTest.kt
+++ app/src/test/java/at/aau/serg/websocketbrokerdemo/core/model/board/TerrainFieldTest.kt
@@ -52,23 +52,30 @@ class TerrainFieldTest {
         terrainField = TerrainField(TerrainType.GRASS, id)
         //if id<0 or id>399, the code should return -1
         if(id < 0 || id > 399){
-            assertEquals(1, terrainField.getNeighbours(id))
-        }
-        when(id){
-            //only the first and last field should have 2 neighbours
-            0, 399 -> assertEquals(2,terrainField.getNeighbours(id).size)
+            assertEquals(1, terrainField.getNeighbours(id).size)
+        }else {
+            when (id) {
+                //only the first and last field should have 2 neighbours
+                0, 399 -> assertEquals(2, terrainField.getNeighbours(id).size)
 
-            //upper right and down left have three, as well as half of all the left/right edges
-            19, 380, 40, 80, 120, 160, 200, 240, 280, 320, 360, 39, 79, 119, 159, 199, 239, 279, 319, 359 -> assertEquals(3, terrainField.getNeighbours(id).size)
+                //upper right and down left have three, as well as half of all the left/right edges
+                19, 380, 40, 80, 120, 160, 200, 240, 280, 320, 360, 39, 79, 119, 159, 199, 239, 279, 319, 359 -> assertEquals(
+                    3,
+                    terrainField.getNeighbours(id).size
+                )
 
-            //upper and lower edges have four neighbours (except for the corners)
-            in 1..18,in 381..398 -> assertEquals(4, terrainField.getNeighbours(id).size)
+                //upper and lower edges have four neighbours (except for the corners)
+                in 1..18, in 381..398 -> assertEquals(4, terrainField.getNeighbours(id).size)
 
-            //the other half of the left/right edges has five neighbours
-            20, 59, 60, 99, 100, 139, 140, 179, 180, 219, 220, 259, 260, 299, 300, 339, 340, 379 -> assertEquals(5, terrainField.getNeighbours(id).size)
+                //the other half of the left/right edges has five neighbours
+                20, 59, 60, 99, 100, 139, 140, 179, 180, 219, 220, 259, 260, 299, 300, 339, 340, 379 -> assertEquals(
+                    5,
+                    terrainField.getNeighbours(id).size
+                )
 
-            //if not in this category, it should have normal 6 neighbours
-            else -> assertEquals(6, terrainField.getNeighbours(id).size)
+                //if not in this category, it should have normal 6 neighbours
+                else -> assertEquals(6, terrainField.getNeighbours(id).size)
+            }
         }
 
     }
