f7d56248ed57cd61004d7507ef01dc4c3f018aea
Mking2001 <1312mkn@gmail.com>
2025-05-07 16:19:54 +0200

    Added Tests in TerrainCardTest class

--- app/src/test/java/at/aau/serg/websocketbrokerdemo/core/model/cards/TerrainCardTest.kt
+++ app/src/test/java/at/aau/serg/websocketbrokerdemo/core/model/cards/TerrainCardTest.kt
@@ -4,6 +4,7 @@ import at.aau.serg.websocketbrokerdemo.core.model.board.TerrainType
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.mock
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class TerrainCardTest {
 
@@ -16,5 +17,18 @@ class TerrainCardTest {
         testCard = TerrainCard("1","Test Card", TerrainType.GRASS)
         assertEquals("1", testCard.id)
         assertEquals("Test Card", testCard.name)
+    }
+
+    @Test
+    fun testValidTerrainCardCreation() {
+        val card = TerrainCard("t1", "Forest", TerrainType.FOREST)
+        assertEquals(TerrainType.FOREST, card.terrainType)
+    }
+
+    @Test
+    fun testInvalidTerrainCardCreation() {
+        assertFailsWith<AssertionError> {
+            TerrainCard("t2","Water", TerrainType.WATER)
+        }
     }
 }
