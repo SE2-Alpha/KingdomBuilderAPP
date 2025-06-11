ce156e54a8621d2d6fd7ef683971905f876d12cc
noemi-presser <noemihannah.presser@gmail.com>
2025-06-11 09:03:32 +0200

    android manifest: put GameEndingActivity to landscape mode +initialized GameEndingScreen method

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/GameEndingActivity.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/GameEndingActivity.kt
@@ -1,9 +1,11 @@
 package at.aau.serg.websocketbrokerdemo
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import at.aau.serg.websocketbrokerdemo.core.model.player.PlayerScoreDTO
 import at.aau.serg.websocketbrokerdemo.ui.theme.MyApplicationTheme
 
 class GameEndingActivity : ComponentActivity() {
@@ -21,4 +24,26 @@ class GameEndingActivity : ComponentActivity() {
 
         }
     }
+}
+
+@Composable
+fun GameEndingScreen(scores: List<PlayerScoreDTO>){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        BackgroundWithTitle()
+    }
+}
+
+@Preview(
+    name = "Landscape Preview",
+    showBackground = true,
+    widthDp = 640,
+    heightDp = 360,
+    uiMode = Configuration.ORIENTATION_LANDSCAPE
+)
+@Composable
+fun GameEndingScreenPreview(){
+    GameEndingScreen(listOf())
 }
