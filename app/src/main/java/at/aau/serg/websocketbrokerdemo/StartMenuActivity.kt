c5e841d4f1b976ed513bd7fa58827fcc9b2c79e6
Viktoris <viktoris@edu.aau.at>
2025-06-11 22:41:30 +0200

    Cleanup imports and comments in StartMenuActivity

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/StartMenuActivity.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/StartMenuActivity.kt
@@ -16,18 +16,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -106,9 +100,6 @@ fun StartMenu() {
             modifier = Modifier
                 .align(Alignment.BottomEnd)
         ) {
-
-            //für Settings und Regeln Composable functions oder eigene Acitvities??
-
             Column {
                 IconButton(
                     onClick = {
