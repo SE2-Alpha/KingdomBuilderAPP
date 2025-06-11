7a6260223b85b75f11d7a5846bc0ec85dfdf600f
Viktoris <viktoris@edu.aau.at>
2025-06-11 19:17:27 +0200

    Make connect automatic

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/StartMenuActivity.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/StartMenuActivity.kt
@@ -2,6 +2,7 @@ package at.aau.serg.websocketbrokerdemo
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -24,6 +25,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -42,6 +46,14 @@ class StartMenuActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        //automatic connection
+        MyStomp.connect(context = this) {
+            MyStomp.subscribeToTopic("/topic/hello-response") { msg ->
+                Log.d("STOMP", "Received: $msg")
+            }
+        }
+
         setContent {
             StartMenu()
         }
