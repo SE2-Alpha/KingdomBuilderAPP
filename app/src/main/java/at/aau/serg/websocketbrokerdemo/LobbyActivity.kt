59da641b1172749db79971a693d4ee18a36d29a2
Roman Lipicer <rolipicer@edu.aau.at>
2025-06-23 23:08:51 +0200

    Fixed Lobby auto join

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/LobbyActivity.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/LobbyActivity.kt
@@ -375,6 +375,12 @@ class LobbyActivity : ComponentActivity()  {
                     )
                     if(MyStomp.playerId == playerObj.getString("id") && isActivityReallyActive())
                     {
+                        players.forEach { player ->
+                            if (player.id == MyStomp.playerId) {
+                                Player.localPlayer = Player(player.id, player.name, player.color)
+                            }
+                        }
+
                         if(RoomStatus.valueOf(obj.getString("status")) == RoomStatus.FINISHED)
                         {
                             MyStomp.leaveRoom(roomId = joinedRoomId.toString())
