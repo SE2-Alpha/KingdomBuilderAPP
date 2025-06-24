59cf875338cfebed62baeff846e679dfd68990fa
Leonardo <leonardotocci6@gmail.com>
2025-06-25 00:38:05 +0200

    Fixed issue: moved return out of when

--- app/src/main/java/at/aau/serg/kingdombuilder/core/model/board/TerrainField.kt
+++ app/src/main/java/at/aau/serg/kingdombuilder/core/model/board/TerrainField.kt
@@ -25,8 +25,9 @@ data class TerrainField(var type: TerrainType, val id: Int, var builtBy: Player?
      */
     fun getNeighbours(id: Int): Array<Int>{
         if (id > -1 && id < 400) {
-            when (id) {
-                20, 60, 100, 140, 180, 220, 260, 300, 340 -> return arrayOf(
+
+           return when (id) {
+                20, 60, 100, 140, 180, 220, 260, 300, 340 -> arrayOf(
                     id - 20,
                     id - 19,
                     id + 1,
@@ -34,19 +35,19 @@ data class TerrainField(var type: TerrainType, val id: Int, var builtBy: Player?
                     id + 21
                 )
 
-                40, 80, 120, 160, 200, 240, 280, 320, 360 -> return arrayOf(
+                40, 80, 120, 160, 200, 240, 280, 320, 360 -> arrayOf(
                     id - 20,
                     id + 1,
                     id + 20
                 )
 
-                39, 79, 119, 159, 199, 239, 279, 319, 359 -> return arrayOf(
+                39, 79, 119, 159, 199, 239, 279, 319, 359 -> arrayOf(
                     id - 20,
                     id - 1,
                     id + 20
                 )
 
-                59, 99, 139, 179, 219, 259, 299, 339, 379 -> return arrayOf(
+                59, 99, 139, 179, 219, 259, 299, 339, 379 -> arrayOf(
                     id - 21,
                     id - 20,
                     id - 1,
@@ -54,14 +55,14 @@ data class TerrainField(var type: TerrainType, val id: Int, var builtBy: Player?
                     id + 20
                 )
 
-                in 1..18 -> return arrayOf(id - 1, id + 1, id + 19, id + 20)
-                in 381..398 -> return arrayOf(id - 1, id + 1, id - 19, id - 20)
-                0 -> return arrayOf(1, 21)
-                19 -> return arrayOf(18, 38, 39)
-                380 -> return arrayOf(360, 361, 381)
-                399 -> return arrayOf(379, 398)
+                in 1..18 ->  arrayOf(id - 1, id + 1, id + 19, id + 20)
+                in 381..398 ->  arrayOf(id - 1, id + 1, id - 19, id - 20)
+                0 ->  arrayOf(1, 21)
+                19 ->  arrayOf(18, 38, 39)
+                380 ->  arrayOf(360, 361, 381)
+                399 ->  arrayOf(379, 398)
 
-                in 21..38, in 61..78, in 101..118, in 141..158, in 181..198, in 221..238, in 261..278, in 301..318, in 341..378 -> return arrayOf(
+                in 21..38, in 61..78, in 101..118, in 141..158, in 181..198, in 221..238, in 261..278, in 301..318, in 341..378 ->  arrayOf(
                     id - 20,
                     id - 19,
                     id - 1,
@@ -71,7 +72,7 @@ data class TerrainField(var type: TerrainType, val id: Int, var builtBy: Player?
                 )
 
                 else -> {
-                    return arrayOf(id - 21, id - 20, id - 1, id + 1, id + 19, id + 20)
+                     arrayOf(id - 21, id - 20, id - 1, id + 1, id + 19, id + 20)
                 }
             }
         }
