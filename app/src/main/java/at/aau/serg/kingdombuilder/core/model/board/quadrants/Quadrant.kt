8be3b0e7113b243cd095aa8b71ace198c58defa0
Leonardo <leonardotocci6@gmail.com>
2025-06-25 00:52:48 +0200

    Fixed issue: removed redundant semicolon

--- app/src/main/java/at/aau/serg/kingdombuilder/core/model/board/quadrants/Quadrant.kt
+++ app/src/main/java/at/aau/serg/kingdombuilder/core/model/board/quadrants/Quadrant.kt
@@ -3,5 +3,5 @@ package at.aau.serg.kingdombuilder.core.model.board.quadrants
 import at.aau.serg.kingdombuilder.core.model.board.TerrainType
 
 fun interface Quadrant {
-    fun getFieldType(id: Int): TerrainType;
+    fun getFieldType(id: Int): TerrainType
 }
